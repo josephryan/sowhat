8585537c32e2d48564671bdc924bdf60e0af460b
Joseph Ryan <josephryan@yahoo.com>
2014-01-09 09:56:51 -0500

    changed references from sowh.pl to sowhat

--- examples.sh
+++ examples.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The following are a set of tests that can be used to make sure
-# sowh.pl is running correctly and provide examples 
+# sowhat is running correctly and provide examples 
 # 
 # To make tests go faster add --reps=10
 # or --rax='raxmlHPC-PTHREADS -T3'
@@ -15,37 +15,37 @@ mkdir -p test.output
 # TEST1: Nucleotide data under GTRGAMMA without partitions
 # constraint tree is much less likely than tree w/highest likelihood
 # p-value should be 0 or close to 0
-sowh.pl --constraint=examples/H1.tre --aln=examples/nt.phy --model=GTRGAMMA --dir=test.output/test1 --name=nt.H1
+sowhat --constraint=examples/H1.tre --aln=examples/nt.phy --model=GTRGAMMA --dir=test.output/test1 --name=nt.H1
 
 # TEST2:
 # constraint tree is a viable hypothesis but not the tree w/highest likelihood
 # p-value should be > 0.05 
-sowh.pl --constraint=examples/H0.tre --aln=examples/nt.phy --model=GTRGAMMA --dir=test.output/test2 --name=nt.H0
+sowhat --constraint=examples/H0.tre --aln=examples/nt.phy --model=GTRGAMMA --dir=test.output/test2 --name=nt.H0
 
 # TEST3:
 # this is a case where the SOWH test fails to provide a consistent answer
 # as to whether the constraint tree is a viable hypothesis 
 # p-value will vary (run several times and you will see)
-sowh.pl --constraint=examples/H2.tre --aln=examples/nt.phy --model=GTRGAMMA --dir=test.output/test3 --name=nt.H2
+sowhat --constraint=examples/H2.tre --aln=examples/nt.phy --model=GTRGAMMA --dir=test.output/test3 --name=nt.H2
 
 # TEST4: Amino acid data under PROTGAMMAWAG with partitions
 # constraint tree is much less likely than tree w/highest likelihood
 # p-value should be 0 or close to 0
-sowh.pl --constraint=examples/H1.tre --aln=examples/aa.phy --partition=examples/aa.partitions --model=PROTGAMMAWAG --dir=test.output/test4 --name=aa.H1.parts
+sowhat --constraint=examples/H1.tre --aln=examples/aa.phy --partition=examples/aa.partitions --model=PROTGAMMAWAG --dir=test.output/test4 --name=aa.H1.parts
 
 # TEST5: Nucleotide data under GTRCAT with partitions
 # constraint tree is much less likely than tree w/highest likelihood
 # p-value should be 0 or close to 0
-sowh.pl --constraint=examples/H1.tre --aln=examples/nt.phy --partition=examples/nt.partitions --model=GTRCAT --dir=test.output/test5 --name=nt.H1.parts
+sowhat --constraint=examples/H1.tre --aln=examples/nt.phy --partition=examples/nt.partitions --model=GTRCAT --dir=test.output/test5 --name=nt.H1.parts
 
 # TEST6: Binary character data 
 # constraint tree is much less likely than tree w/highest likelihood
 # p-value should be 0 or close to 0
-sowh.pl --constraint=examples/H1.tre --aln=examples/char.phy --model=MULTIGAMMA --dir=test.output/test6 --name=char.H1
+sowhat --constraint=examples/H1.tre --aln=examples/char.phy --model=MULTIGAMMA --dir=test.output/test6 --name=char.H1
 
 # TEST7: Nucleotide data using PhyloBayes for the null distribution
-sowh.pl --constraint=examples/H1.tre --aln=examples/nt.phy --model=GTRGAMMA --dir=test.output/test1 --name=nt.H1.pb --usepb
+sowhat --constraint=examples/H1.tre --aln=examples/nt.phy --model=GTRGAMMA --dir=test.output/test1 --name=nt.H1.pb --usepb
 
 # CURRENT TEST - CHAINS and RECALCULATE:
-sowh.pl --constraint=examples/H1.tre --aln=examples/nt.phy --model=GTRGAMMA --dir=test.output/test1 --name=nt.H1   --chains=2 --reps=100 --recalculate
+sowhat --constraint=examples/H1.tre --aln=examples/nt.phy --model=GTRGAMMA --dir=test.output/test1 --name=nt.H1   --chains=2 --reps=100 --recalculate
 
