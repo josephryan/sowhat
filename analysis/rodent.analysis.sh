8b4bd1599541c6a1e1b6a8e6dc9835a407faec76
shchurch <samuelchurch@Samuels-MacBook-Air-2.local>
2014-04-24 23:07:46 -0400

    small adjustment to published data

--- analysis/rodent.analysis.sh
+++ analysis/rodent.analysis.sh
@@ -9,7 +9,7 @@ module load phylobayes/3.3f;
 
 #Test 1 - runs=100 reps=100
 
-perl sowhat --constraint=published_datasets/Sullivan12S.t2.tre --aln=published_datasets/Sullivan12S.phy --model=GTRGAMMA --dir=/users/shchurch/scratch/sull.test1 --name=sull.test1 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.sull.test1 --reps=100 --runs=100
+perl sowhat --constraint=published_datasets/Sullivan12S.t1.tre --aln=published_datasets/Sullivan12S.phy --model=GTRGAMMA --dir=/users/shchurch/scratch/sull.test1 --name=sull.test1 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.sull.test1 --reps=100 --runs=100
 
 #Plot p-values
 
@@ -18,11 +18,11 @@ R CMD BATCH --no-save --no-restore '--args analysis/sull.test1' analysis/stats.r
 
 #Test 2 - runs=4 reps=500
 
-perl sowhat --constraint=published_datasets/Sullivan12S.t2.tre --aln=published_datasets/Sullivan12S.phy --model=GTRGAMMA --dir=/users/shchurch/scratch/sull.test2 --name=sull.test2 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.sull.test2 --reps=500 --runs=4
+perl sowhat --constraint=published_datasets/Sullivan12S.t1.tre --aln=published_datasets/Sullivan12S.phy --model=GTRGAMMA --dir=/users/shchurch/scratch/sull.test1 --name=sull.test1 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.sull.test1 --reps=500 --runs=4
 
 #Test 3 - runs=100 reps=500
 
-perl sowhat --constraint=published_datasets/Sullivan12S.t2.tre --aln=published_datasets/Sullivan12S.phy --model=GTRGAMMA --dir=/users/shchurch/scratch/sull.test3 --name=sull.test3 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.sull.test3 --reps=500 --runs=100
+perl sowhat --constraint=published_datasets/Sullivan12S.t1.tre --aln=published_datasets/Sullivan12S.phy --model=GTRGAMMA --dir=/users/shchurch/scratch/sull.test3 --name=sull.test3 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.sull.test3 --reps=500 --runs=100
 
 #Plot p-values
 
@@ -31,7 +31,7 @@ R CMD BATCH --no-save --no-restore '--args analysis/sull.test3' analysis/stats.r
 
 #Test 4 - runs=100 reps=500 rerun
 
-perl sowhat --constraint=published_datasets/Sullivan12S.t2.tre --aln=published_datasets/Sullivan12S.phy --model=GTRGAMMA --dir=/users/shchurch/scratch/sull.test4 --name=sull.test4 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.sull.test4 --reps=500 --runs=100 --rerun
+perl sowhat --constraint=published_datasets/Sullivan12S.t1.tre --aln=published_datasets/Sullivan12S.phy --model=GTRGAMMA --dir=/users/shchurch/scratch/sull.test4 --name=sull.test4 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.sull.test4 --reps=500 --runs=100 --rerun
 
 #Plot p-values
 
@@ -40,7 +40,7 @@ R CMD BATCH --no-save --no-restore '--args analysis/sull.test4' analysis/stats.r
 
 #Test 5 - runs=100 reps=500 usepb
 
-perl sowhat --constraint=published_datasets/Sullivan12S.t2.tre --aln=published_datasets/Sullivan12S.phy --model=GTRGAMMA --dir=/users/shchurch/scratch/sull.test5 --name=sull.test5 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.sull.test5 --reps=500 --runs=100 --usepb
+perl sowhat --constraint=published_datasets/Sullivan12S.t1.tre --aln=published_datasets/Sullivan12S.phy --model=GTRGAMMA --dir=/users/shchurch/scratch/sull.test5 --name=sull.test5 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.sull.test5 --reps=500 --runs=100 --usepb
 
 #Plot p-values
 
