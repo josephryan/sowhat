88d8dbc403a67d8c41ef4ec97df377ea968a0e81
shchurch <samuelchurch@Samuels-MacBook-Air-2.local>
2014-05-09 14:20:32 -0400

    Version 0.16 Changed analysis files, stop
    Stopping Criterion is now an option. Analysis updated.

--- analysis/analysis.sh
+++ analysis/analysis.sh
@@ -11,6 +11,11 @@ module load phylobayes/3.3f;
 
 perl sowhat --constraint=published_datasets/Sullivan12S.t2.tre --aln=published_datasets/Sullivan12S.phy --model=GTRGAMMA --dir=/users/shchurch/scratch/sull.test1 --name=sull.test1 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.sull.test1 --reps=500 --runs=100
 
+#Plot p-values
+
+grep p-value: /users/shchurch/scratch/sull.test1/sowhat.results.* >analysis/sull.test1.pvals
+R CMD BATCH --no-save --no-restore '--args analysis/sull.test1' analysis/stats.r
+
 # TEST 2: model effects, using CAT model with PhyloBayes for simulation
 
 perl sowhat --constraint=published_datasets/Sullivan12S.t2.tre --aln=published_datasets/Sullivan12S.phy --model=GTRGAMMA --dir=/users/shchurch/scratch/sull.test2 --name=sull.test2 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.sull.test2 --reps=500 --usepb
@@ -25,12 +30,19 @@ perl sowhat --constraint=published_datasets/Sullivan12S.t2.tre --aln=published_d
 
 perl sowhat --constraint=published_datasets/castoe.t1.tre --aln=published_datasets/castoe2009.phy --partition=published_datasets/castoe.parts.txt --model=GTRGAMMA --dir=/users/shchurch/scratch/castoe.test1 --name=castoe.test1 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.castoe.test1 --reps=500 --runs=20
 
+#Plot p-values
+
+grep p-value: /users/shchurch/scratch/castoe.test1/sowhat.results.* >analysis/castoe.test1.pvals
+R CMD BATCH --no-save --no-restore '--args analysis/castoe.test1' analysis/stats.r
+
 # TEST 2: model effects, using CAT model with PhyloBayes for simulation
 #		 partition effects as well
 
-perl sowhat --constraint=published_datasets/castoe.t1.tre --aln=published_datasets/castoe2009.phy --partition=published_datasets/castoe.parts.txt --model=GTRGAMMA --dir=/users/shchurch/scratch/castoe.test2.0 --name=castoe.test2.0 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.castoe.test2.0 --reps=500 --usepb
+perl sowhat --constraint=published_datasets/castoe.t1.tre --aln=published_datasets/castoe2009.phy.reduced --partition=published_datasets/castoe.parts.txt.reduced --model=GTRGAMMA --dir=/users/shchurch/scratch/castoe.test2.0 --name=castoe.test2.0 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.castoe.test2.0 --reps=500 --usepb
 
-perl sowhat --constraint=published_datasets/castoe.t1.tre --aln=published_datasets/castoe2009.phy --model=GTRGAMMA --dir=/users/shchurch/scratch/castoe.test2.1 --name=castoe.test2.1 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.castoe.test2.1 --reps=100
+perl sowhat --constraint=published_datasets/castoe.t1.tre --aln=published_datasets/castoe2009.phy --model=GTRGAMMA --dir=/users/shchurch/scratch/castoe.test2.1 --name=castoe.test2.1 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.castoe.test2.1 --reps=500
+
+perl sowhat --constraint=published_datasets/castoe.t1.tre --aln=published_datasets/castoe2009.phy --model=GTRGAMMA --dir=/users/shchurch/scratch/castoe.test2.2 --name=castoe.test2.2 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.castoe.test2.2 --usepb --reps=500
 
 # TEST 3: RAxML consistent optimization
 
@@ -45,6 +57,11 @@ perl sowhat --constraint=published_datasets/castoe.t1.tre --aln=published_datase
 # TEST 1: sample size using LG/GTRUNLINKED
 
 perl sowhat --constraint=published_datasets/protein.t1.tre --aln=published_datasets/proteic_M1384_11x391_2003.phy --model=PROTGAMMALG --dir=/users/shchurch/scratch/prot.test1 --name=prot.test1 --rax=~/data/shchurch/SOWH/src/raxmlHPC --seqgen=~/data/shchurch/SOWH/src/Seq-Gen.v1.3.3/source/seq-gen >output.prot.test1 --reps=500 --runs=20
+
+#Plot p-values
+
+grep p-value: /users/shchurch/scratch/prot.test1/sowhat.results.* >analysis/prot.test1.pvals
+R CMD BATCH --no-save --no-restore '--args analysis/prot.test1' analysis/stats.r
 
 # TEST 2: model effects using LG/CAT
 
